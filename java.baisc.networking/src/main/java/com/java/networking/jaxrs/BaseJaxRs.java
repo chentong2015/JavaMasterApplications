1b56e36b933b3f4e95565807a889020a7664bc29
tchen <tchen@murex.com>
2022-04-06 16:57:46 +0200

    enrich base jax rs

--- java.baisc.networking/src/main/java/com/java/networking/jaxrs/BaseJaxRs.java
+++ java.baisc.networking/src/main/java/com/java/networking/jaxrs/BaseJaxRs.java
@@ -2,21 +2,40 @@ package com.java.networking.jaxrs;
 
 import jakarta.ws.rs.client.Client;
 import jakarta.ws.rs.client.ClientBuilder;
-import jakarta.ws.rs.client.ClientRequestContext;
 import jakarta.ws.rs.client.WebTarget;
+import jakarta.ws.rs.sse.InboundSseEvent;
+import jakarta.ws.rs.sse.SseEventSource;
 
+import java.util.concurrent.TimeUnit;
+
+// TODO. JAX-RS: javax restful web service
+//       提供后端API的实现, 同时包含Client API
+// https://gayerie.dev/epsi-poe-201703/javaee/11_jaxrs.html
+// https://github.com/jakartaee/rest  jakarta.ws.rs
+
+// TODO. JAX-RS bug的地方 ?
+// JAX-RS Spec mandates that when the connection is lost, the reconnect MUST occur.
+// But it is not possible to recognize when the connection is lost and when closed.
+// This leads the JAX-RS client to re-connect whenever the server completes sending events !!
 public class BaseJaxRs {
-
-    private ClientRequestContext requestContext;
 
     // Jax.Rs Client 发送请求，获取到指定类型的返回数据
     public void testJaxRsClient() {
         Client client = ClientBuilder.newClient();
-        WebTarget target = client.target("http://www.server.net/person");
+        WebTarget endpoint = client.target("http://www.server.net/person");
         // Person person = target.request().get(Person.class);
 
-        // 可以从请求的Context中获取请求的相关信息
-        requestContext.getUri();
-        requestContext.getHeaders();
+        long reconnectMillis = 100;
+        SseEventSource sseEventSource = SseEventSource
+                .target(endpoint)
+                .reconnectingEvery(reconnectMillis, TimeUnit.MILLISECONDS).build();
+        // 使用sseEventSource来注册，在error发生和完成的时候执行相应的逻辑 !!
+        // sseEventSource.register(inboundSseEvent -> handleMessage(inboundSseEvent, onEvent));
+        // sseEventSource.register(inboundSseEvent -> handleMessage(inboundSseEvent, onEvent), onError);
+        // sseEventSource.register(inboundSseEvent -> handleMessage(inboundSseEvent, onEvent), onError, onComplete);
+    }
+
+    public void handleMessage(InboundSseEvent inboundSseEvent) {
+        String data = inboundSseEvent.readData();
     }
 }
---
+++ java.baisc.networking/src/main/java/com/java/networking/jaxrs/MyClientRequestFilter.java
@@ -0,0 +1,18 @@
+package com.java.networking.jaxrs;
+
+import jakarta.ws.rs.client.ClientRequestContext;
+import jakarta.ws.rs.client.ClientRequestFilter;
+
+import java.io.IOException;
+
+public class MyClientRequestFilter implements ClientRequestFilter {
+
+    // 对客户端的请求进行过滤
+    @Override
+    public void filter(ClientRequestContext clientRequestContext) throws IOException {
+        String token = (String) clientRequestContext.getHeaders().getFirst("Authorization");
+        if (token != null) {
+            System.out.println("valid");
+        }
+    }
+}
