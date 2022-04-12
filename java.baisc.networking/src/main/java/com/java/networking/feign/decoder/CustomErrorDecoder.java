0026b44a2dfa576ffb023d6eb51429ab2ae6f641
tchen <tchen@murex.com>
2022-04-12 17:25:24 +0200

    add feign http response handler

--- java.baisc.networking/src/main/java/com/java/networking/feign/decoder/CustomErrorDecoder.java
+++ java.baisc.networking/src/main/java/com/java/networking/feign/decoder/CustomErrorDecoder.java
@@ -8,14 +8,12 @@ import feign.codec.ErrorDecoder;
 public class CustomErrorDecoder implements ErrorDecoder {
 
     // TODO. 自定义实现Feign client retry on exception
-    // 根据请求return status code, 返回指定的Retryable异常, 用于retryer的处理逻辑
+    // 根据请求return status code, 返回指定的Retryable重试异常, 用于retryer的处理逻辑 !!
     @Override
     public Exception decode(String methodKey, Response response) {
         FeignException exception = feign.FeignException.errorStatus(methodKey, response);
-        // 可以从response中获取到到请求的URL
-        String url = response.request().url();
-        int status = response.status();
-        if (status >= 500) {
+        if (HttpResponseHandler.shouldRetryAfter(response)) {
+            // RetryableException有两种构造器可以创建实例
             return new RetryableException(
                     response.status(),
                     exception.getMessage(),
