b9deba5cebaa11d901ad6877d62e6270c8317d42
tchen <tchen@murex.com>
2022-04-20 15:20:42 +0200

    add Google Core Libraries For Java

---
+++ java.basic.others/src/main/java/com/java/others/CommonIO/GoolgeGuavaCoreJava.java
@@ -0,0 +1,8 @@
+package com.java.others.CommonIO;
+
+// Guava: Google Core Libraries For Java
+// Expanded libraries that include utility classes, Google's collections, I/O classes, and much more
+// 集合以及IO的扩展类库
+public class GoolgeGuavaCoreJava {
+    
+}
